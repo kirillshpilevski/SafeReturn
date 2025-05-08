a7400f70be06bc494157f0dc5bd8cf0195d09396
Kirill Shpilevski <shpilevskikirill@outlook.com>
2025-05-08 14:00:00 +0000

    feat: add threshold management

--- src/interfaces/IApprovalModule.sol
+++ src/interfaces/IApprovalModule.sol
@@ -4,7 +4,11 @@ pragma solidity ^0.8.20;
 interface IApprovalModule {
     event RequestApproved(uint256 indexed requestId, address indexed approver);
     event RequestRejected(uint256 indexed requestId, address indexed approver);
+    event ThresholdUpdated(uint256 newThreshold);
+    event FastTrackLimitUpdated(uint256 newLimit);
 
     function approveRequest(uint256 requestId) external;
     function rejectRequest(uint256 requestId) external;
+    function isApproved(uint256 requestId) external view returns (bool);
+    function getApprovalCount(uint256 requestId) external view returns (uint256);
 }
