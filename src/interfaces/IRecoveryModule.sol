61302cef7875fc98a43135dc6abed9dc4ed89972
Kirill Shpilevski <shpilevskikirill@outlook.com>
2025-05-10 15:00:00 +0000

    feat: add emergency return function

--- src/interfaces/IRecoveryModule.sol
+++ src/interfaces/IRecoveryModule.sol
@@ -6,8 +6,10 @@ import {Types} from "../libraries/Types.sol";
 interface IRecoveryModule {
     event VaultFrozen(address indexed guardian);
     event VaultThawed(address indexed guardian);
+    event EmergencyReturn(uint256 indexed requestId, address indexed safeAddress);
 
     function freezeVault() external;
     function thawVault() external;
+    function emergencyReturn(uint256 requestId, address safeAddress) external;
     function getRecoveryState() external view returns (Types.RecoveryState);
 }
