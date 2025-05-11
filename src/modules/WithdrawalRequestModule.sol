a60fcee751c54a67534d4d7c15fa06708a5ed959
Kirill Shpilevski <shpilevskikirill@outlook.com>
2025-05-11 13:00:00 +0000

    feat: add module initialization

--- src/modules/WithdrawalRequestModule.sol
+++ src/modules/WithdrawalRequestModule.sol
@@ -3,11 +3,19 @@ pragma solidity ^0.8.20;
 
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {Types} from "../libraries/Types.sol";
+import {IWithdrawalRequestModule} from "../interfaces/IWithdrawalRequestModule.sol";
 
-contract WithdrawalRequestModule is Initializable {
+contract WithdrawalRequestModule is Initializable, IWithdrawalRequestModule {
     uint256 private _nextRequestId;
     mapping(uint256 => Types.WithdrawalRequest) private _requests;
+    mapping(address => uint256[]) private _userRequests;
 
     uint256 public minDelay;
     uint256 public maxDelay;
+
+    function __WithdrawalRequestModule_init(uint256 _minDelay, uint256 _maxDelay) internal onlyInitializing {
+        minDelay = _minDelay;
+        maxDelay = _maxDelay;
+        _nextRequestId = 1;
+    }
 }
